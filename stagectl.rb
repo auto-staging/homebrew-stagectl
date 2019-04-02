3cf0ea4d50fd179d21b619dccd5b3cfc3e68cf62
Jan Ritter <jan.ritter@trustedshops.de>
2019-04-02 09:55:24 +0200

    fix: replaced name of formula with correct one

--- stagectl.rb
+++ stagectl.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 
-class Terrastate < Formula
+class Stagectl < Formula
     desc "Stagectl is the CLI tool to manage your Auto Staging setup"
     url "https://github.com/janritter/terrastate/releases/download/1.2.0/darwin_amd64_terrastate"
     sha256 "0a72b10b19deb74d2a09e18f85b1b2bde9258331ded07aa7578a2b789bd07b08"
