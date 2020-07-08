1d98297f3b6b11a5a06caf8815803a613c6adfe7
Travis CI User <travis@example.org>
2020-07-08 17:38:18 +0000

    chore: deployment of version 1.2.0 - 2020-07-08_17:36:27

--- stagectl.rb
+++ stagectl.rb
@@ -4,7 +4,7 @@
 class Stagectl < Formula
     desc "Stagectl is the CLI tool to manage your Auto Staging setup"
     url "https://github.com/auto-staging/stagectl/releases/download/1.2.0/darwin_amd64_stagectl"
-    sha256 "52cdecc1f14d4db41d1f8fe6ccd9a94015aa55b79749a73eff7c316dc6b79f74"
+    sha256 "e851e0fac882a795391f7e4d46e2d59d98da6571f72c66276883b51dfff12331"
   
     def install
       bin.install "darwin_amd64_stagectl"
